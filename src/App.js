85eb2a8d9ce9d55fc1da54c024fb5c492d1b91c4
Adam Round <adamround@hotmail.com>
2020-05-05 12:27:51 +0100

    adding all files

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "./App.css";
+import { Container, FormGroup, Form, Input, Label, Button } from "reactstrap";
+import axios from "axios";
+import socketIOClient from "socket.io-client";
+// const serverURI = "https://enigmatic-sierra-05542.herokuapp.com/";a
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      message: "",
+      messages: [],
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.socket = socketIOClient();
+
+    this.socket.on("message", () => this.getMessages());
+    this.socket.on("hello", (message) => console.log(message));
+  }
+
+  componentDidMount() {
+    this.getMessages();
+    console.log("mounted");
+  }
+
+  getMessages() {
+    axios
+      .get("/messages")
+      .then((res) =>
+        this.setState({
+          messages: res.data.map((message) => (
+            <div
+              className={
+                message.name === this.state.name
+                  ? "message-container-own"
+                  : "message-container-other"
+              }
+            >
+              <div
+                className={
+                  message.name === this.state.name
+                    ? "speach-bubble-own"
+                    : "speach-bubble-other"
+                }
+              >
+                <p>{message.message}</p>
+              </div>
+              <div
+                className={
+                  message.name === this.state.name ? "name-own" : "name-other"
+                }
+              >
+                {message.name}
+              </div>
+            </div>
+          )),
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  handleSubmit(e) {
+    if (e.key === "Enter" || !e.key) {
+      e.preventDefault();
+      axios.post("/messages", this.state);
+      this.setState({ message: "" });
+    }
+  }
+
+  handleDelete() {
+    axios.delete("/messages").catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container className="themed-container border shadow p-3 rounded ">
+          <Button
+            color="danger"
+            className="delete-button"
+            onClick={this.handleDelete}
+          >
+            Delete All Messages
+          </Button>
+          <div className="messages">{this.state.messages}</div>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input
+                type="text"
+                placeholder="Insert screen name here."
+                value={this.state.name}
+                onChange={(e) => this.setState({ name: e.target.value })}
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="input message">Message</Label>
+              <Input
+                type="textarea"
+                placeholder="Write your message here."
+                value={this.state.message}
+                onChange={(e) => this.setState({ message: e.target.value })}
+                onKeyDown={this.handleSubmit}
+              ></Input>
+            </FormGroup>
+            <Button type="submit">Send Message</Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
